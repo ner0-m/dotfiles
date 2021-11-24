7603e468ce345c13ddeb6217e31b9c1f465abfc4
David Frank <frankd@in.tum.de>
2021-11-25 00:14:21 +0100

    Update globals

--- .config/nvim/lua/neva/globals.lua
+++ .config/nvim/lua/neva/globals.lua
@@ -1,16 +1,16 @@
-require("neva.plugins") 
+require("neva.plugins")
 require("neva.build")
- 
+
 P = function(v)
-    print(vim.inspect(v))
-    return v
+	print(vim.inspect(v))
+	return v
 end
 
 RELOAD = function(...)
-    return require("plenary.reload").reload_module(...)
+	return require("plenary.reload").reload_module(...)
 end
 
 R = function(name)
-    RELOAD(name)
-    return require(name)
+	RELOAD(name)
+	return require(name)
 end
--- .config/nvim/lua/neva/init.lua
+++ .config/nvim/lua/neva/init.lua
@@ -1,16 +1,3 @@
 require("neva.plugins")
 require("neva.build")
-
-P = function(v)
-	print(vim.inspect(v))
-	return v
-end
-
-RELOAD = function(...)
-	return require("plenary.reload").reload_module(...)
-end
-
-R = function(name)
-	RELOAD(name)
-	return require(name)
-end
+require("neva.globals")
